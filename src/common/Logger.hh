3eb84b310ffcbbaa978d12b8d8442a7793303d23
Thai Nhat Minh <t.n.minh1997@gmail.com>
2021-08-29 16:48:06 +0700

    * Add info to macro logger

--- src/common/Logger.hh
+++ src/common/Logger.hh
@@ -13,11 +13,11 @@ public:
 #define STR_HELPER(x) #x
 #ifndef LOG_TO_FILE
 #define LOG std::cout
-#define LOG_DEBUG std::cout
-#define LOG_WARN std::cout
-#define LOG_ERROR std::cout
-#define LOG_INFO std::cout
-#define LOG_API std::cout << "API:"
+#define LOG_DEBUG std::cout << "[DEBUG]:"
+#define LOG_WARN std::cout << "[WARN]:"
+#define LOG_ERROR std::cout << "[ERROR]:"
+#define LOG_INFO std::cout << "[INFO]:"
+#define LOG_API std::cout << "[API]:"
 #define TRACE std::cout << __FUNCTION__ ":"  << __LINE__ << "\n"
 #else
 #define LOG Log().Message()
@@ -25,6 +25,6 @@ public:
 #define LOG_ERROR Log().Message() << "[ERR]: "
 #define LOG_INFO Log().Message() << "[INFO]: "
 #define LOG_DEBUG Log().Message() << "[DEBUG]: "
-#define LOG_API Log().Message() << "API:"
+#define LOG_API Log().Message() << "[API]:"
 #define TRACE Log().Message() << __FUNCTION__  << std::endl
 #endif
