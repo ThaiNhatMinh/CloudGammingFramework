25f06d66089825e37a922b8bf0717a4a4cb3592b
Thai Nhat Minh <t.n.minh1997@gmail.com>
2021-09-05 04:41:07 +0700

    + Add method to create named event and share memmory

--- src/ipc/Named.cpp
+++ src/ipc/Named.cpp
@@ -1,5 +1,29 @@
+#include <sstream>
 #include "Named.hh"
 
 const std::string LAUNCH_EVENT = "Local\\LaunchEvent";
 const std::string LAUNCH_EVENT_MEMORY = "Local\\LaunchEventMemory";
 const std::string POLL_PROCESS_EVENT = "Local\\PollProcess";
+const std::string DISCONNECT_TIMER = "Local\\Timer";
+
+
+std::string CreateGameInfoString(ClientId client, GameId id)
+{
+    std::stringstream ss;
+    ss << "Local\\GameInfo_" << client << '_' << id;
+    return ss.str();
+}
+
+std::string CreateDisconnectString(ClientId client, GameId id)
+{
+    std::stringstream ss;
+    ss << "Local\\Disconnect_" << client << '_' << id;
+    return ss.str();
+}
+
+std::string CreateShutdownString(ClientId client, GameId id)
+{
+    std::stringstream ss;
+    ss << "Local\\ShutdownGame_" << client << '_' << id;
+    return ss.str();
+}
--- src/ipc/Named.hh
+++ src/ipc/Named.hh
@@ -1,6 +1,13 @@
 #pragma once
 #include <string>
+#include "cgf/cgf.hh"
 
 extern const std::string LAUNCH_EVENT;
 extern const std::string LAUNCH_EVENT_MEMORY;
 extern const std::string POLL_PROCESS_EVENT;
+extern const std::string DISCONNECT_TIMER;
+extern const std::string SHUTDOWN_EVENT;
+
+std::string CreateDisconnectString(ClientId client, GameId id);
+std::string CreateShutdownString(ClientId client, GameId id);
+std::string CreateGameInfoString(ClientId client, GameId id);
