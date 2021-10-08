dd42677d24df109ca919d0071b373c7dc2fec26d
Thái Nhat Minh <t.n.minh1997@gmail.com>
2021-10-09 05:21:56 +0700

    + Add more ipc named string

--- src/ipc/Named.cpp
+++ src/ipc/Named.cpp
@@ -5,6 +5,9 @@ const std::string LAUNCH_EVENT = "Local\\LaunchEvent";
 const std::string LAUNCH_EVENT_MEMORY = "Local\\LaunchEventMemory";
 const std::string POLL_PROCESS_EVENT = "Local\\PollProcess";
 const std::string DISCONNECT_TIMER = "Local\\Timer";
+const std::string STREAMCONTROLLER_EVENT = "Local\\StreamController";
+const std::string STOPOLL_EVENT = "Local\\StopPoll";
+const std::string SENDFRAME_EVENT = "Local\\SendFrame";
 
 
 std::string CreateGameInfoString(ClientId client, GameId id)
@@ -25,5 +28,19 @@ std::string CreateShutdownString(ClientId client, GameId id)
 {
     std::stringstream ss;
     ss << "Local\\ShutdownGame_" << client << '_' << id;
+    return ss.str();
+}
+
+std::string CreateStreamControllerString(void* ptr)
+{
+    std::stringstream ss;
+    ss << STREAMCONTROLLER_EVENT << ptr;
+    return ss.str();
+}
+
+std::string CreateStopPollString(void *ptr)
+{
+    std::stringstream ss;
+    ss << STOPOLL_EVENT << ptr;
     return ss.str();
 }
--- src/ipc/Named.hh
+++ src/ipc/Named.hh
@@ -7,7 +7,10 @@ extern const std::string LAUNCH_EVENT_MEMORY;
 extern const std::string POLL_PROCESS_EVENT;
 extern const std::string DISCONNECT_TIMER;
 extern const std::string SHUTDOWN_EVENT;
+extern const std::string SENDFRAME_EVENT;
 
 std::string CreateDisconnectString(ClientId client, GameId id);
 std::string CreateShutdownString(ClientId client, GameId id);
 std::string CreateGameInfoString(ClientId client, GameId id);
+std::string CreateStreamControllerString(void* ptr);
+std::string CreateStopPollString(void* ptr);
