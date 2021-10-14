45450daf08844b4d9a1739dc65aeb6f6ce8985b8
Thái Nhat Minh <t.n.minh1997@gmail.com>
2021-10-14 22:36:10 +0700

    * Correct path of example game

--- examples/TestSun.cpp
+++ examples/TestSun.cpp
@@ -12,11 +12,11 @@ int main()
     std::vector<GameParameter> games;
     GameParameter console;
     console.Id = 1;
-    console.ExecuteLocation = "build\\test\\Game\\Debug\\Console.exe";
+    console.ExecuteLocation = "build\\examples\\Game\\Debug\\Console.exe";
     games.push_back(console);
     GameParameter imgui;
     imgui.Id = 2;
-    imgui.ExecuteLocation = "build\\test\\Game\\Debug\\ImGuiOpenGL.exe";
+    imgui.ExecuteLocation = "build\\examples\\Game\\Debug\\ImGuiOpenGL.exe";
     imgui.ClientDisconnectTimeOut = 10 * 1000;
     games.push_back(imgui);
     Sun sun(&config, games);
